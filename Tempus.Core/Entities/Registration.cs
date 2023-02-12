d93997de634ad91ef2785c6638ce0468db6ebcfd
Fanaru Victor <fanaruvictor01@gmail.com>
2023-02-12 15:11:41 +0200

    make title and content not nullabel

--- Tempus.Core/Entities/Registration.cs
+++ Tempus.Core/Entities/Registration.cs
@@ -15,8 +15,8 @@ public class Registration : BaseEntity
 
     public Registration() { }
 
-    public string? Title { get; set; }
-    public string? Content { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime LastUpdatedAt { get; set; }
     public Guid CategoryId { get; set; }
